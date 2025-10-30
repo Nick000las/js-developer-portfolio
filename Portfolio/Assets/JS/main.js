1d5b417286a80b0fd09892cebcbeef5faedffcfc
Nick000las <nick000las.roeder@gmail.com>
2025-10-29 21:03:57 -0300

    Adição dos idiomas

--- Portfolio/Assets/JS/main.js
+++ Portfolio/Assets/JS/main.js
@@ -30,12 +30,17 @@ function updateHardSkills(profileData){
     const hardSkills = document.getElementById('profile.skills.hardSkills');
     hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('');
 }
+function updateLanguages(profileData){
+    const languages = document.getElementById('profilelanguages');
+    languages.innerHTML = profileData.languages.map(language => `<li>${language}</li>`).join('');
+}
 
 (async () => {
     const profileData = await fetchProfileData();
     updateProfileInfo(profileData);
     updateSoftSkills(profileData);
     updateHardSkills(profileData);
+    updateLanguages(profileData);
 })()
 
 
