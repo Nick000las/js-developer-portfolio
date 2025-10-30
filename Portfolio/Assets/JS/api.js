440740aa87fa810e3b9745603715923fb2f613d9
Nick000las <nick000las.roeder@gmail.com>
2025-10-30 16:17:57 -0300

    Alterado o caminho para o index ficar no root

--- Portfolio/Assets/JS/api.js
+++
@@ -1,5 +0,0 @@
-async function fetchProfileData(){
-    const url = 'https://raw.githubusercontent.com/Nick000las/js-developer-portfolio/refs/heads/main/Portfolio/Assets/data/profile.json';
-    const fetching = await fetch(url);
-    return await fetching.json()
-}
